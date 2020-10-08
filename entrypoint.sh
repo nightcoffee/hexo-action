1a20d5deb903d8aed23a1defb3f6010e6d7c64d7
nightcoffee <nightcoffee@users.noreply.github.com>
2020-10-08 17:29:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ set -e
 mkdir -p /root/.ssh/
 echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+ssh-keyscan -t rsa "$INPUT_SSH_HOST" >> /root/.ssh/known_hosts
 
 # setup deploy git account
-git config --global user.name "$INPUT_USER_NAME"
-git config --global user.email "$INPUT_USER_EMAIL"
+# git config --global user.name "$INPUT_USER_NAME"
+# git config --global user.email "$INPUT_USER_EMAIL"
 
 # install hexo env
 npm install hexo-cli -g
